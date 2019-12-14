66b6864a8b12d4a104052d10ce8b468028e35259
Commodity <515171601@qq.com>
2019-12-14 19:54:01 +0800

    Signed-off-by: Commodity Janus_V

--- main.cpp
+++ main.cpp
@@ -2,8 +2,36 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-
+int main(){
+	cout<<"欢迎使用购物篮管理系统!\n";
+	cout<<" 0) 退出系统\n";
+	cout<<" 1) 向购物篮添加商品\n";
+	cout<<" 2) 从购物篮移除商品\n";
+	cout<<" 3) 查看指定商品\n";
+	cout<<" 4) 查看所有商品\n";
+	cout<<" 5) 结算\n";
+	cout<<"请输入功能选项:";
+	char choice;
+	while(true){
+		cin>>choice;
+		if(choice=='0')
+			break;
+		switch(choice) {
+			case '1': //具体功能省略
+				break;
+			case '2': //具体功能省略
+				break;
+			case '3': //具体功能省略
+				break;
+			case '4': //具体功能省略
+				break;
+			case '5': //具体功能省略
+				break;
+			default:
+				cout<<"无效输入! 请重试!\n";
+				break;
+		}
+	}
+	cout<<"再见!\n";
 	return 0;
 }
